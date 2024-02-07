23be299a768cdc63d1c56aef6d1d526db5f0cf36
RamonLewa <ramonloewenstein@gmail.com>
2024-02-07 19:46:20 -0300

    First commit

---
+++ SistemaLogin/CadastroUsuario.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaLogin
+{
+    static class CadastroUsuario
+    {
+        private static Usuario[] usuarios =
+        {
+            new Usuario(){Nome = "Ramon", Senha = "7692"}
+        };
+
+        private static Usuario _userLogado = null;
+
+        public static Usuario UsuarioLogado
+        {
+            get { return _userLogado; }
+            private set { _userLogado = value; }
+        }
+
+        public static bool Login(string Nome, string Senha)
+        {
+            foreach (Usuario usuario in usuarios)
+            {
+                if (usuario.Nome == Nome && usuario.Senha == Senha)
+                {
+                    UsuarioLogado = usuario;
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ SistemaLogin/FormLogin.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaLogin
+{
+    public partial class FormLogin : Form
+    {
+        public static bool Cancelar = false;
+
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSenha_Click(object sender, EventArgs e)
+        {
+            string nome = txtUsuario.Text;
+            string senha = txtSenha.Text;
+            if (CadastroUsuario.Login(nome, senha))
+            {
+                Close();
+            } else
+            {
+                MessageBox.Show("Acesso negado");
+                txtUsuario.Text = "";
+                txtSenha.Text = "";
+                txtUsuario.Focus();
+                Close();
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Cancelar = true;
+            Close();
+        }
+    }
+}
